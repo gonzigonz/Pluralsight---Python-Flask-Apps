37fd4867368ee942f119f148901108b39f39829d
gonzigonz <gonzigonz@users.noreply.github.com>
2016-05-06 08:57:23 +1000

    Forms and View Logic - Demo: Views and Forms

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template, url_for
+from datetime import datetime
+from flask import Flask, render_template, url_for, request, redirect, url_for
+
+from logging import DEBUG
+
 app = Flask(__name__)
+app.logger.setLevel(DEBUG)
+
+bookmarks = []
+
+def store_bookmark(url):
+    bookmarks.append(dict(
+        url = url,
+        user = "gonz",
+        date = datetime.utcnow()
+    ))
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html")
         
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
+    if  request.method == "POST":
+        url = request.form["url"]
+        store_bookmark(url)
+        app.logger.debug("stored url: " + url)
+        return redirect(url_for("index"))
     return render_template("add.html")
     
 @app.errorhandler(404)
