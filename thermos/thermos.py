6df71c99d8428726bebf13e2ae373f6d2857e392
gonzigonz <gonzigonz@users.noreply.github.com>
2016-05-06 08:38:47 +1000

    Basic Templates and Views - Demo: Custom Error Pages

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -9,6 +9,14 @@ def index():
 @app.route("/add")
 def add():
     return render_template("add.html")
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+    
+@app.errorhandler(500)
+def server_error(e):
+    return render_template("500.html"), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
