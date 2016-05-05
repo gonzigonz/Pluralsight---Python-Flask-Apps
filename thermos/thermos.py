baf26d145fddc64329bb00eca5308e49b58db345
gonzigonz <gonzigonz@users.noreply.github.com>
2016-05-05 17:35:33 +1000

    Basic Templates and Views - Demo: Using url_for to Generate Links

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,21 +1,14 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-class User:
-    def __init__(self, firstname, lastname):
-        self.firstname = firstname
-        self.lastname = lastname
-    
-    def initials(self):
-        return "{}. {}.".format(self.firstname[0], self.lastname[0])
-
 @app.route("/")
 @app.route("/index")
-def hello():
-    return render_template(
-        'index.html',
-        title="Title passed from view to template",
-        user=User("Gonzalo", "Lucero"))
+def index():
+    return render_template("index.html")
+        
+@app.route("/add")
+def add():
+    return render_template("add.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
