f2eae2e2f202724dca8da40d4b7b86118e741776
gonzigonz <gonzigonz@users.noreply.github.com>
2016-05-05 17:18:52 +1000

    Basic Templates and Views - Demo: HTML Templates With Jinja2

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+class User:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+    
+    def initials(self):
+        return "{}. {}.".format(self.firstname[0], self.lastname[0])
+
 @app.route("/")
 @app.route("/index")
 def hello():
-    return render_template('index.html')
+    return render_template(
+        'index.html',
+        title="Title passed from view to template",
+        user=User("Gonzalo", "Lucero"))
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
