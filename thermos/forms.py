fd01eaeba354f5342c4d311d514ae80b1b233196
gonzigonz <gonzigonz@users.noreply.github.com>
2016-07-06 19:06:03 +1000

    Forms and View Logic - Demo: Custom Validation

--- thermos/forms.py
+++ thermos/forms.py
@@ -6,3 +6,16 @@ from wtforms.validators import DataRequired, url
 class BookmarkForm(Form):
     url = URLField("url", validators=[DataRequired(), url()])
     description = StringField("description")
+
+    def validate(self):
+        if not self.url.data.startswith("http://") or\
+            self.url.data.startswith("https://"):
+            self.url.data = "http://" + self.url.data
+
+        if not Form.validate(self):
+            return False
+
+        if not self.description.data:
+            self.description.data = self.url.data
+        
+        return True
