b06e58ba038030a8d126841249585521b86a6baa
gonzigonz <gonzigonz@users.noreply.github.com>
2016-07-27 22:00:59 +1000

    Visual Studio Code - Configured debugger

--- thermos/__init__.py
+++ thermos/__init__.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "o\xde\x87&\xf9\xc7\x00hJ*\xe5\x94\xbd\xd3\xef\x8a\xa3D\xa3P\x8b\x1a:]"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "thermos.db")
-app.config["DEBUG"] = True
+app.config["DEBUG"] = False
 db = SQLAlchemy(app)
 
 import models
